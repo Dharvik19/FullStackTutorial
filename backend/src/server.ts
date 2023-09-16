67666a145568b7241e30465d7418b663eba53d22
Dharvik19 <dharvik.badga22@gmail.com>
2023-09-16 20:01:07 +0530

    connecting to mongodb

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,13 @@
-import express from "express";
-const app = express();
-const port = 5000;
+import app from "./app";
+import mongoose from "mongoose";
+import env from "./util/validateEnv";
 
+const port = env.PORT;
 
-app.get("/", (req, res)=>{
-    res.send("HEllo world");
-})
-
-app.listen(port,()=>{
-    console.log("Server running on port "+ port);
-});
+mongoose.connect(env.MONGO_CONNECTION_STRING)
+.then(()=>{
+    console.log("mongoose connected");
+    app.listen(port,()=>{
+        console.log("Server running on port "+ port);
+    });
+}).catch(console.error);
